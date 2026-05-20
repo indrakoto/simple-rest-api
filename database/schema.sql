a2f1411d2376995709775d64872c6a55abc4d1c1
Koto <indrakoto@gmail.com>
2026-05-20 19:06:50 +0700

    update schema, menambhakan notes dan berita

--- database/schema.sql
+++ database/schema.sql
@@ -18,3 +18,33 @@ CREATE TABLE `users` (
   KEY `users_ibfk_1` (`role_id`),
   CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+CREATE TABLE `kategori` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_kategori` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE berita (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    kategori_id INT NOT NULL,
+    judul VARCHAR(255) NOT NULL,
+    isi TEXT NOT NULL,
+    gambar VARCHAR(255) NULL,
+    created_at DATE NOT NULL,
+    updated_at DATE NOT NULL,
+    FOREIGN KEY (kategori_id) REFERENCES kategori(id)
+);
+
+
+CREATE TABLE notes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    judul VARCHAR(255) NOT NULL,
+    isi TEXT NOT NULL,
+    created_at DATE NOT NULL,
+    updated_at DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
